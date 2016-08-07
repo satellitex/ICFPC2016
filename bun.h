88d367f1f2b1052a61c3cf94d161dd6d41ea353c
satellitex <s.a.t.e.3.ths@gmail.com>
2016-08-07 23:22:13 +0900

    update bun.h

--- bun.h
+++ bun.h
@@ -1,7 +1,10 @@
 #include<bits/stdc++.h>
+#include<boost/multiprecision/cpp_int.hpp>
+namespace mp = boost::multiprecision;
+typedef mp::cpp_int ll;
 using namespace std;
 
-typedef long long ll;
+
 struct Bun{ // x/y
   ll x,y;
   Bun(){ x = 0; y = 1; }
@@ -12,8 +15,8 @@ struct Bun{ // x/y
     x = (ll)a; y=1000000000LL;
     kiyaku();
   }
-  void kiyaku(){
-    ll c = __gcd( abs(x), abs(y) );
+  void kiyaku(){    
+    ll c = gcd( abs(x), abs(y) );
     x/=c; y/=c;
     if( x < 0 && y < 0 ) { x*=-1; y*=-1; }
     else if( y < 0 ) { x *= -1; y *= -1; }
@@ -27,4 +30,47 @@ struct Bun{ // x/y
     string s; ss >> s;
     return s;
   }
+  
+  Bun operator + (const Bun& b) const {
+    ll t = lcm( y, b.y );    
+    ll a = x * ( t / y );
+    ll c = b.x * ( t / b.y );  
+    return Bun( a + c , t );
+  }
+  Bun& operator += (const Bun& b ) {
+    *this = *this + b;
+    return *this;
+  }
+  
+  Bun operator - (const Bun& b) const {
+    return *this + Bun( -b.x, b.y );
+  }
+  Bun& operator -= (const Bun& b) {
+    *this = *this - b;
+    return *this;
+  }
+  
+  Bun operator * (const Bun& b) const {
+    return Bun( x * b.x , y * b.y );
+  }
+  Bun& operator *= (const Bun& b) {
+    *this = *this * b;
+    return *this;
+  }
+  
+  Bun operator / (const Bun& b) const {
+    return Bun( x * b.y , y * b.x );
+  }
+  Bun& operator /= (const Bun& b) {
+    *this = *this / b;
+    return *this;
+  }
+
 };
+
+ostream &operator<<(ostream &out, const Bun &tgt)
+{
+  out << tgt.x << "/" << tgt.y;
+  return out;
+}
+
